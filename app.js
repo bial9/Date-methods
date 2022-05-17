92f16b5b80edc989bce89f13f7a3a266a7270f6a
Bilal <99763904+bial9@users.noreply.github.com>
2022-05-18 01:39:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// var a = new Date()
+// console.log(a);
+
+// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var a = new Date();
+// var b = months[a.getMonth()];
+// alert(b);
+
+// var days = ["sun","mon","tue","wed","thu","fri","sat"]
+// var a = new Date();
+// var b = days[a.getDay()]
+// console.log(b);
+
+// if (b === "sat" || "sun") {
+//     alert("its fun day")
+// }
+
+// var a = new Date();
+// var b = a.getDate()
+// // document.write(b)
+
+// if (b<=15) {
+//     document.write("First fifteen days")
+// }
+// if (b>=15) {
+//     document.write("last fifteen days")
+// }
+
+// var a = new Date();
+// document.write(a)
+// var c = a.setTime()
+// var b = a.getMilliseconds()
+// document.write(c)
+
+// function date(){
+//     var a = new Date();
+//     document.write(a)
+//     return a;
+// }
+// date()
+
+// function name(){
+//     var a = prompt("enter first name");
+//     var b = prompt("enter last name");
+//     document.write(a+" "+b);
+// }
+// name()
+
+// function sum(){
+//     var a = +prompt("num1")
+//     var b = +prompt("num2")
+//     document.write(a+b)
+// }
+// sum()
+
+// function calculator(){
+//     var a = +prompt("num1")
+//     var b = +prompt("operator")
+//     var c = +prompt("num2")
+//     var d = a+b+c
+//     document.write(d)
+
+//     if (b === "+") {
+//         document.write(a+b)
+//     }
+//     if (b === "-") {
+//         document.write(a-b)
+//     }
+//     if (b === "/") {
+//         document.write(a/b)
+//     }
+//     if (b === "*") {
+//         document.write(a*b)
+//     }
+//     document.write(a+b+c)
+// }
+// calculator()
